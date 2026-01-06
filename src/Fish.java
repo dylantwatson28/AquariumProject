c12eafac2aa75dc6482229d23e923bf6f1d04230
dylanwatson28 <dylan_watson28@milton.edu>
2026-01-06 11:24:39 -0500

    Initial commit

---
+++ src/Fish.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+    public class Fish {
+        public String name;
+        public int xpos;
+        public int ypos;
+        public int dx;
+        public int dy;
+        public int width;
+        public int height;
+        public boolean isAlive;
+        public Rectangle hitbox;
+
+        public Fish(int pXpos, int pYpos){
+            xpos = pXpos;
+            ypos = pYpos;
+            dx = 10;
+            dy = 10;
+            width = 60;
+            height = 60;
+            isAlive = true;
+            hitbox = new Rectangle(xpos,ypos,width,height);
+        }
+        public void move(){
+            if(xpos>=1000 - width){
+                dx=-dx;
+            }
+            if(xpos<=0){
+                dx=-dx;
+            }
+            if(ypos>=700){
+                dy=-dy;
+            }
+            if(ypos<=0){
+                dy=-dy;
+            }
+            xpos=xpos+dx;
+            ypos=ypos+dy;
+            hitbox = new Rectangle(xpos,ypos,width,height);
+        }
+    }
